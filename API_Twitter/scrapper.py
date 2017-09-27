70c0fef67bedec1314d97775725351478e0fc982
Guilhermeslucas <guilherme.slucas@gmail.com>
2017-09-27 17:26:24 -0300

    first version working

--- API_Twitter/scrapper.py
+++ API_Twitter/scrapper.py
@@ -3,5 +3,21 @@
 import tweepy
 
 #put your information here
-acces_keys = {'consumer_key':'','consumer_secret':'',
-            'access_token':'','access_token_secret':''}
+access_keys = {'consumer_key':'',
+            'consumer_secret':'',
+            'access_token':'',
+            'access_token_secret':''}
+
+def get_timeline(screen_name, count):
+    auth = tweepy.OAuthHandler(access_keys['consumer_key'], access_keys['consumer_secret'])
+    auth.set_access_token(access_keys['access_token'],access_keys['access_token_secret'])
+    api = tweepy.API(auth)
+
+    new_tweets = api.user_timeline(screen_name = screen_name,count=count)
+
+    for tweet in new_tweets:
+        print(tweet.text)
+        print(' ')
+
+if __name__ == '__main__':
+    get_timeline('@realDonaldTrump', 200)
